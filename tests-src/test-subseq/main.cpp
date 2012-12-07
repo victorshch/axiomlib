8599cab6ec6c96fdaa21cb1739afe701c4eda547
armkor <armkor@08e97c73-d1ae-4023-87e5-8c5be0462271>
2012-12-07 12:01:46 +0000

    Empty message
    git-svn-id: https://svn.lvk.cs.msu.su/axiomlib/branches/armkor@195 08e97c73-d1ae-4023-87e5-8c5be0462271

---
+++ tests-src/test-subseq/main.cpp
@@ -0,0 +1,13 @@
+#include "FuzzyMultiDataExt/ASStageMultiMarking.h"
+#include "MultiMarking/MetricsEqual.h"
+
+
+#include <iostream>
+
+using namespace std;
+using namespace AxiomLib;
+
+int main(){
+
+    return 0;
+}
