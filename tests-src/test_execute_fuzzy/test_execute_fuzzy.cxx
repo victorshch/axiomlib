a8da3c4a2c3753066d67973e4de58abd07086569
wictor <wictor@08e97c73-d1ae-4023-87e5-8c5be0462271>
2013-03-19 18:16:11 +0000

    axiomlib: test_execute_fuzzy (the utility for running axiomsets) now works
    This utility now loads axiomsets form XML files.
    
    Signed-off-by: Victor Shcherbinin <victorshch@gmail.com>
    
    git-svn-id: https://svn.lvk.cs.msu.su/axiomlib/trunk@205 08e97c73-d1ae-4023-87e5-8c5be0462271

--- tests-src/test_execute_fuzzy/test_execute_fuzzy.cxx
+++ tests-src/test_execute_fuzzy/test_execute_fuzzy.cxx
@@ -1,6 +1,6 @@
 #include "Environment.h"
 #include "AxiomLibException.h"
-#include "FuzzyMultiDataLearnAlgorithm.h"
+#include "FuzzyMultiDataExtAlgorithm.h"
 #include "AxiomExprSet.h"
 #include "AxiomExprSetPlus.h"
 #include <iostream>
@@ -11,30 +11,54 @@
 #undef SEEK_SET
 #undef SEEK_END
 #undef SEEK_CUR
-#include "mpi.h"
 
 using namespace AxiomLib;
 
 #define comments true
 #define defaultConfigFile	"./axiomlib_exf.conf"
 
+int loadAxoimExprSet (const std::string baseDir, const std::string fileName,
+					  const std::vector <std::string>& dataSetParams, AxiomExprSetPlus& aesp) {
+	// Инициализация системы аксиом из указанного файла
+	AxiomSetBase axiomSetBase;
+	AxiomExprSetStructure aess;
+	std::string axiomSetPath = baseDir;
+	axiomSetPath.append("/");
+	axiomSetPath.append(fileName);
+	// Считываем информацию о системе аксиом в aess
+	axiomSetBase.readFromAS (axiomSetPath, aess, dataSetParams);
+	aesp.initAxiomSetFromStructure (aess);
+
+	return 0;
+}
+
+std::set<std::string> getAxiomSetNames(const std::string& axiomSetNameTemplate, const std::string& axiomSetDir) {
+	std::set<std::string> all_matching_files;
+
+	boost::filesystem::directory_iterator end_itr; // Default ctor yields past-the-end
+	for( boost::filesystem::directory_iterator i(axiomSetDir); i != end_itr; ++i )
+	{
+		// Skip if not a file
+		if( !boost::filesystem::is_regular_file( i->status() ) ) continue;
+
+		std::string currentAxiomName = i->path().stem();
+
+		if(currentAxiomName.compare(0, axiomSetNameTemplate.size(), axiomSetNameTemplate) != 0) {
+			continue;
+		}
+
+		// File matches, store it
+		all_matching_files.insert( currentAxiomName );
+	}
+
+	return all_matching_files;
+}
+
 int main (int argc, char** argv) {
 	try {
-		MPI_Init(&argc, &argv);
-		int size, rank = 0;
-		MPI_Comm_size(MPI_COMM_WORLD, &size);
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-		std::cout << "\n	process  " << rank << " size " <<  size << "\n";
-
-		srand( (unsigned int) (time(0) + rank*1111));
-
-		if ((comments) && (rank == 0)) {
-			std::cout << "\n" << "AxiomLib / Execute AxiomSet on Fuzzy data.\n	Reading config-file." << "\n";
-			std::cout << "To continue press enter.";
-			std::cin.get();
-		}	
-
-		MPI_Barrier (MPI_COMM_WORLD); 
+		if ((comments)) {
+			std::cout << "\n" << "AxiomLib / Execute AxiomSet on Fuzzy data.\n" << std::endl;
+		}
 
 		Environment envStart;
 		envStart.readConfigParams (argc, argv);
@@ -51,28 +75,39 @@ int main (int argc, char** argv) {
 		// Параметры, по которым сохранять лучщие решения
 		std::string axiomSetBaseDir;
 		if (env.getStringParamValue(axiomSetBaseDir, "AxiomSetBaseDir") < 0)
-			throw AxiomLibException("FuzzyMultiDataLearnAlgorithm::setParamsFromEnv : axiomSetBaseDir directory is undefined.");
+			throw AxiomLibException("Execute_fuzzy : axiomSetBaseDir directory is undefined.");
 		std::set <std::string> axiomSetNames;
-		if (env.getStringSetParamValue(axiomSetNames, "AxiomSet") < 0)
-			throw AxiomLibException("FuzzyMultiDataLearnAlgorithm::setParamsFromEnv : AxiomSet is undefined.");
+		if (env.getStringSetParamValue(axiomSetNames, "AxiomSet") < 0) {
+			std::string axiomSetNameTemplate;
+			if (env.getStringParamValue(axiomSetNameTemplate, "AxiomSetNameTemplate")) {
+				throw AxiomLibException("Execute_fuzzy : no AxiomSet specified");
+			}
+			axiomSetNames = getAxiomSetNames(axiomSetNameTemplate, axiomSetBaseDir);
+		}
 
-		FuzzyMultiDataLearnAlgorithm fuzzyMultiDataLearnAlgorithm;
-		fuzzyMultiDataLearnAlgorithm.initFromEnvRecognitionOnly(env);
-		fuzzyMultiDataLearnAlgorithm.setComments(comments);
+		FuzzyMultiDataExtAlgorithm fuzzyMultiDataExtAlgorithm;
+		fuzzyMultiDataExtAlgorithm.initFromEnvRecognitionOnly(env);
+		fuzzyMultiDataExtAlgorithm.setComments(comments);
+
+//		std::vector<std::string> dataSetParams = fuzzyMultiDataLearnAlgorithm.getDataSet().getParamNames();
+
+		std::vector<std::string> asNames;
+		for (std::set<std::string>::const_iterator c_iter = axiomSetNames.begin();
+			 c_iter != axiomSetNames.end(); ++c_iter) {
+			asNames.push_back(*c_iter);
+		}
+
+		fuzzyMultiDataExtAlgorithm.loadAxiomSetsFromXml(asNames);
 
 		std::cout << "\nResults are:";
-		int errFirst, errSecond;
-		double goal;
-		int i = 1;
-		for (std::set<std::string>::const_iterator c_iter = axiomSetNames.begin(); c_iter != axiomSetNames.end(); ++c_iter) {
-			goal = fuzzyMultiDataLearnAlgorithm.matterAxiomSetFunc (axiomSetBaseDir, *c_iter, errFirst, errSecond);
-			std::cout << "\n\t" << i <<".\tAxiomSet: " << *c_iter << "\t" << goal << " (" << errFirst << ", " << errSecond << ").";
-			i++;
-		}
-		
-		MPI_Barrier (MPI_COMM_WORLD);
 
-		MPI_Finalize ();
+		for(int i = 0; i < (int)fuzzyMultiDataExtAlgorithm.getASSize(); ++i) {
+			AxiomExprSetPlus& aess = fuzzyMultiDataExtAlgorithm.getAS(i);
+			fuzzyMultiDataExtAlgorithm.recalculateMatterAxiomSetFunc(aess);
+			std::cout << "\n\t" << i <<".\tAxiomSet: " << aess.name() << "\t"
+					  << aess.goal << " (" << aess.errFirst << ", " << aess.errSecond << ").";
+
+		}
 
 		return 0;
 
@@ -80,7 +115,6 @@ int main (int argc, char** argv) {
 	catch ( AxiomLibException testex ) {
 		std::cerr << "Fatal toplevel AxiomLibException:\n\t";
 		std::cerr << testex.error() << "\n";
-		MPI_Finalize ();
 		return -1;
 	}
 }
