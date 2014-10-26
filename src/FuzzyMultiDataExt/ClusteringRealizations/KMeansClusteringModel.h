9a998a89db9e382d85ac8e1e89122a9df8770ea1
Tims101 <tims101@yandex.ru>
2014-10-26 19:50:48 +0400

    asgenetics_251014 clusterisation serialization

--- src/FuzzyMultiDataExt/ClusteringRealizations/KMeansClusteringModel.h
+++ src/FuzzyMultiDataExt/ClusteringRealizations/KMeansClusteringModel.h
@@ -10,6 +10,11 @@
 #include <shark/Models/Normalizer.h>
 #include "ClusteringModel.h"
 #include "../../AxiomLibException.h"
+#include "boost/serialization/assume_abstract.hpp"
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/nvp.hpp"
+
+
 
 namespace AxiomLib {
 
@@ -37,6 +42,19 @@ protected:
 	void setParam(std::string name, std::string value);
 
 private:
+
+	//объ€влени€, необходимые дл€ сериализации
+	friend class boost::serialization::access;
+
+	//функци€ сериализации - в ней должны быть указаны члены-данные,
+	//характеризующие состо€ние объекта
+	template<class Archive>
+	void serialize(Archive & archive, const unsigned int /*version*/)
+	{
+		archive & BOOST_SERIALIZATION_BASE_OBJECT_NVP(ClusteringModel);
+		centroids.write(archive);
+	}
+
 	shark::Centroids centroids;
 	std::vector<shark::RealVector> data;
 	std::vector<std::vector<double> > store;
